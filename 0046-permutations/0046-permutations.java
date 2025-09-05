e084e60bf0ac9a9849e981a7238713b14ccd9bad
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2025-09-05 18:40:52 +0530

    Time: 1 ms (92.67%), Space: 44.5 MB (64.3%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,25 +2,49 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        backtrack(nums, new ArrayList<>(), new boolean[nums.length], result);
+        // backtrack(nums, new ArrayList<>(), new boolean[nums.length], result);
+        swapping(nums, 0, result);
         return result;
     }
 
     public void backtrack(int[] nums, List<Integer> temp, boolean[] used, List<List<Integer>> result) {
-        if(temp.size() == nums.length) {
+        if (temp.size() == nums.length) {
             result.add(new ArrayList<>(temp));
             return;
         }
-        for(int i=0;i<nums.length;i++) {
-            if(!used[i]) {
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
                 used[i] = true;
                 temp.add(nums[i]);
 
                 backtrack(nums, temp, used, result);
 
-                temp.remove(temp.size()-1);
+                temp.remove(temp.size() - 1);
                 used[i] = false;
             }
         }
+    }
+
+    public void swapping(int[] nums, int idx, List<List<Integer>> result) {
+        if (idx >= nums.length) {
+            List<Integer> temp = new ArrayList<>(nums.length);
+            for (int num : nums) {
+                temp.add(num);
+            }
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = idx; i < nums.length; i++) {
+            swap(nums, i, idx); // Do
+            swapping(nums, idx + 1, result); // Explore
+            swap(nums, i, idx); // Undo
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
