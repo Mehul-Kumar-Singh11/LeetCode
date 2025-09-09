f87c37ea39c621c89d0f1872bb9ba1afcc715e67
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2025-09-09 22:15:23 +0530

    Time: 17 ms (86.09%), Space: 94.8 MB (42.38%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,23 +1,39 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-      List<List<Integer>> result = new ArrayList<>();
-      List<Integer> temp = new ArrayList<>();
-      backtrack(1, n, k, temp, result);  
-      return result;
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        // backtrackApproach1(1, n, k, temp, result);
+        backtrackApproach2(1, n, k, temp, result);
+        return result;
     }
-    public void backtrack(int start, int n, int k, List<Integer> temp, List<List<Integer>> result) {
-        if(k==0) {
-            result.add(new ArrayList<Integer>(temp));
-            return;
-        }
-        // Base Case
-        if(start > n) {
+
+    // public void backtrackApproach1(int start, int n, int k, List<Integer> temp, List<List<Integer>> result) {
+    //     if (k == 0) {
+    //         result.add(new ArrayList<Integer>(temp));
+    //         return;
+    //     }
+    //     // Base Case
+    //     if (start > n) {
+    //         return;
+    //     }
+    //     // take or skip approach
+    //     temp.add(start);
+    //     backtrackApproach1(start + 1, n, k - 1, temp, result);
+    //     temp.remove(temp.size() - 1);
+    //     backtrackApproach1(start + 1, n, k, temp, result);
+    // }
+
+    // using for loop
+    public void backtrackApproach2(int start, int n, int k, List<Integer> temp, List<List<Integer>> result) {
+        if (k == 0) {
+            result.add(new ArrayList<>(temp));
             return;
         }
 
-        temp.add(start);
-        backtrack(start+1, n, k-1, temp, result);
-        temp.remove(temp.size()-1);
-        backtrack(start+1, n, k, temp, result);
+        for (int i = start; i <= n; i++) {
+            temp.add(i);
+            backtrackApproach2(i + 1, n, k - 1, temp, result);
+            temp.remove(temp.size() - 1);
+        }
     }
 }
