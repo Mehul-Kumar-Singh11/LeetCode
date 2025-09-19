76c16802c79f035d59b886e6fb1a4cd4ffd2c4e3
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2025-09-20 02:25:44 +0530

    Time: 81 ms (19.35%), Space: 60.7 MB (53.66%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -5,14 +5,17 @@ class Solution {
 
         int n = temperatures.length;
         for(int i=n-1;i>=0;i--) {
+            // pop all indexes with a lower or equal temperature until find a warmer temperature
             while(!st.isEmpty() && temperatures[i] >= temperatures[st.peek()]) {
                 st.pop();
             }
 
+            // if stack is not empty, means found a warmer temperature
             if(!st.isEmpty()) {
                 result[i] = st.peek() - i;
             }
 
+            // insert current index in the stack
             st.push(i);
         }
 
