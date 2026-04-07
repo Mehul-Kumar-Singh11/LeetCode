918d259caf321cb97c0272349c13a52394c2a0b7
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2026-04-08 00:44:19 +0530

    Time: 0 ms (100%), Space: 42.6 MB (41.91%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,29 +1,21 @@
-import java.util.LinkedList;
-
 class Solution {
     public boolean isHappy(int n) {
-
         int slow = n;
         int fast = n;
-
-        do {
-            slow = square(slow);
-            fast = square(square(fast));
-        } while (slow != fast);
+       do {
+            slow = calculateSquaresSum(slow);
+            fast = calculateSquaresSum(calculateSquaresSum(fast));
+       } while(slow != fast);
 
         return slow == 1;
     }
-
-    public int square(int num) {
-
-        int ans = 0;
-
-        while (num > 0) {
-            int remainder = num % 10;
-            ans += remainder * remainder;
-            num /= 10;
+    public int calculateSquaresSum(int n)  {
+        int sum = 0;
+        while(n > 0) {
+            int digit = n % 10;
+            n = n  / 10;
+            sum = sum + (digit * digit);
         }
-
-        return ans;
+        return sum;
     }
 }
