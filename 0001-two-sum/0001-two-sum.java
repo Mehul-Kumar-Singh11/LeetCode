26042dda0201daf39bdd4f8d612a144549ba3ec6
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2026-04-11 00:45:58 +0530

    Time: 2 ms (99.28%), Space: 47.4 MB (5.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         // have to return the indices so we cannot use sorting
-        // using two loops
         int n = nums.length;
+        /*
+        // Brute Force Approach - using two loops
         for(int i=0;i<n-1;i++) {
             for(int j=i+1;j<n;j++) {
                 int sum = nums[i]+nums[j];
@@ -10,6 +11,17 @@ class Solution {
                     return new int[]{i, j};
                 }
             }
+        }
+        */
+        // Optimized Approach - using HashMap
+        HashMap<Integer,Integer> hmap = new HashMap<>();
+        // insert element value and index in hashmap
+        for(int i=0;i<n;i++) {
+            int complement = target - nums[i];
+            if(hmap.containsKey(complement)) {
+                return new int[]{hmap.get(complement), i};
+            }
+            hmap.put(nums[i], i);
         }
         return new int[]{}; 
     }
