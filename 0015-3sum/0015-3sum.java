85a361dfcb6a5c281ba5c6b040baa33873944c9f
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2026-04-14 16:38:36 +0530

    Time: 35 ms (43.32%), Space: 59.4 MB (27%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        int target = 0;
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        for(int i=0;i<n-2;i++) {
+            if(i>0 && nums[i] == nums[i-1]) {
                 continue;
             }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+            int j = i+1;
+            int k = n-1;
+            while(j<k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == target) {
+                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
-                    while (j < k && nums[j] == nums[j - 1]) {
-                        j++;
-                    }
-                    while (j < k && nums[k] == nums[k + 1]) {
-                        k--;
-                    }
+                    // skip duplicates  for j and k
+                    while(j<k && nums[j]==nums[j-1]) { j++; }
+                    while(j<k && nums[k]==nums[k+1]) { k--; }
+                }
+                else if(sum < target) {
+                    j++;
+                }
+                else {
+                    k--;
                 }
             }
         }
-        return res;
+        return list;
     }
 }
