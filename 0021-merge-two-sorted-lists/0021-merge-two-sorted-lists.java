af713cc3371a6c7142a91d259178d0202843fab5
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2026-04-14 15:05:51 +0530

    Time: 0 ms (100%), Space: 44.2 MB (78.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,34 +8,35 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+import java.util.LinkedList;
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        // Pushing the values of the first linked list
-        while (list1 != null) {
-            arr.add(list1.val);
-            list1 = list1.next;
-        }
-
-        // Pushing the values of the second linked list
-        while (list2 != null) {
-            arr.add(list2.val);
-            list2 = list2.next;
-        }
-
-        // Sorting the list
-        Collections.sort(arr);
-
-        // Creating a new list with sorted values
         ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
-
-        for (int i = 0; i < arr.size(); i++) {
-            curr.next = new ListNode(arr.get(i));
-            curr = curr.next;
+        ListNode current = dummy;
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+        while(temp1 != null && temp2 != null) {
+            if(temp1.val > temp2.val) {
+                current.next = temp2;
+                temp2 = temp2.next;
+            }
+            else {
+                current.next = temp1;
+                temp1 = temp1.next;
+            }
+            current = current.next;
         }
-
+        
+        while(temp1 != null) {
+            current.next = temp1;
+            temp1 = temp1.next;
+            current = current.next;
+        }
+        while(temp2 != null) {
+            current.next = temp2;
+            temp2 = temp2.next;
+            current = current.next;
+        }
         return dummy.next;
     }
 }
