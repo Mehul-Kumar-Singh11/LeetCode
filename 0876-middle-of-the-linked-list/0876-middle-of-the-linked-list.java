94a3cbeedfd0a0c851a1ebaa54f5219dec435d10
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2026-04-20 14:34:41 +0530

    Time: 0 ms (100%), Space: 43 MB (19.86%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,38 +10,12 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if (head == null)
-            return null;
         ListNode slow = head;
         ListNode fast = head;
-        while (fast != null && fast.next != null) {
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
         return slow;
     }
-
-    // Brute Approach
-    /* 
-     public ListNode middleNode(ListNode head) {
-        int size = getSize(head);
-        int halfSize = ( size / 2) + 1;
-        int temp = 1;
-        ListNode curr = head;
-        while(temp != halfSize) {
-            curr = curr.next;
-            temp++;
-        }
-        return curr;
-    }
-    public int getSize(ListNode head) {
-        ListNode curr = head;
-        int size = 0;
-        while(curr != null) {
-            size++;
-            curr = curr.next;
-        }
-        return size;
-    }
-    */
 }
