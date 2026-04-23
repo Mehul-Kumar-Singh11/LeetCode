9fadc86384face4a3a625ad5bcd95e7b3ee2c305
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2026-04-24 01:17:51 +0530

    Time: 1 ms (98.57%), Space: 47.5 MB (24.79%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,6 +1,8 @@
 class Solution {
     public int pivotIndex(int[] a) {
         int n = a.length;
+        // Brute Force Approach
+        /*
         int[] prefix = new int[n];
         int[] suffix = new int[n];
         prefix[0] = 0;
@@ -11,12 +13,34 @@ class Solution {
         for(int j=n-2;j>=0;j--) {
             suffix[j] = suffix[j+1] + a[j+1];
         }
-
         for(int z=0;z<n;z++){
             if(prefix[z] == suffix[z]) {
                 return z;
             }
         }
         return -1;
+        */
+        // Optimized Approach
+        int left_sum = 0;
+        int right_sum = 0;
+        int sum = calculate_sum(a, n);
+        for (int i = 0; i < n; i++) {
+            if (i > 0) {
+                left_sum = left_sum + a[i - 1];
+            }
+            right_sum = sum - a[i] - left_sum;
+            if (left_sum == right_sum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int calculate_sum(int[] a, int n) {
+        int sum = 0;
+        for (int val : a) {
+            sum += val;
+        }
+        return sum;
     }
 }
