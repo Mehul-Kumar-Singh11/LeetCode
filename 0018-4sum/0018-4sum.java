73f14b1d32fcd2944bcfcfa286b81ba5f9bd9bb8
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2026-06-07 21:09:21 +0530

    Time: 22 ms (20.24%), Space: 45.8 MB (45.6%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,28 +1,29 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums); // -2 -1 0 0 1 2 -> -2 -1 0 2, -2 -1 
         int n = nums.length;
         List<List<Integer>> al = new ArrayList<>();
+        int[] tempNums = Arrays.copyOf(nums, n);
+        Arrays.sort(tempNums);
         for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+            if (i > 0 && tempNums[i] == tempNums[i - 1]) {
                 continue;
             }
             for (int j = i + 1; j < n - 2; j++) {
-                if (j > i+1 && nums[j] == nums[j - 1]) {
+                if (j > i + 1 && tempNums[j] == tempNums[j - 1]) {
                     continue;
                 }
                 int k = j + 1;
                 int l = n - 1;
                 while (k < l) {
-                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    long sum = (long)tempNums[i] + tempNums[j] + tempNums[k] + tempNums[l];
                     if (sum == target) {
-                        al.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        al.add(Arrays.asList(tempNums[i], tempNums[j], tempNums[k], tempNums[l]));
                         k++;
                         l--;
-                        while (k < l && nums[k] == nums[k - 1]) {
+                        while (k < l && tempNums[k] == tempNums[k - 1]) {
                             k++;
                         }
-                        while (k < l && nums[l] == nums[l + 1]) {
+                        while (k < l && tempNums[l] == tempNums[l + 1]) {
                             l--;
                         }
                     } else if (sum < target) {
